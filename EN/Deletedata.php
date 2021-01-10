fcb1a135f9ec63e28f9fe8142ada0a9ab54cf905
ArashAmini345 <75423754+ArashAmini345@users.noreply.github.com>
2021-01-10 16:30:34 +0330

    Add files via upload

---
+++ EN/Deletedata.php
@@ -0,0 +1,3 @@
+<?php
+header('location: /AA sys/delete_EN.php');
+?>
---
+++ EN/index.php
@@ -0,0 +1,4 @@
+<h1>Sorry! this language has been now creating!</h1><br>
+<form action="Deletedata.php" method="POST">
+            <input class="FARSICLASS" type="submit" value="Delete Data">
+</form>
